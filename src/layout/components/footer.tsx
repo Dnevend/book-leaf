56c1dfe901bf7c2aa8a78dc49dd6e4c62b80485c
Dnevend <Dnevend@gmail.com>
2024-04-17 15:22:28 +0800

    feat: user config menubar

--- src/layout/components/footer.tsx
+++ src/layout/components/footer.tsx
@@ -1,12 +1,47 @@
+import { LogOut, UserCog } from "lucide-react";
 import { Logo } from "@/components/global";
+import { Button } from "@/components/ui";
+import { useSupabaseAuth } from "@/provider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const { isAuth, signOut, userInfo } = useSupabaseAuth();
+
   return (
     <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl w-full mx-auto md:px-8">
-      <div className="items-center justify-between sm:flex">
-        <div className="mt-4 sm:mt-0">
-          &copy; 2024 BookLeaf All rights reserved.
-        </div>
+      <div className="items-center justify-between flex">
+        {isAuth && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                <UserCog />
+                <span className="sr-only">User Config</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                {userInfo?.email || "My Account"}
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to={"/me"}>Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={signOut}>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
 
         <Logo size="sm" />
       </div>
--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -1,7 +1,4 @@
 import { useNavigate } from "react-router";
-import { ProfileForm } from "./components/profileForm";
-import { Button } from "@/components/ui";
-import { LogOut } from "lucide-react";
 import { useSupabaseAuth } from "@/provider/supabaseAuth";
 import { useEffect } from "react";
 import BgLibrary from "@/assets/bg_library_1.jpg";
@@ -9,36 +6,26 @@ import BgLibrary from "@/assets/bg_library_1.jpg";
 const Mine = () => {
   const navigate = useNavigate();
 
-  const { isAuth, userInfo, signOut } = useSupabaseAuth();
+  const { isAuth, userInfo } = useSupabaseAuth();
 
   useEffect(() => {
     if (!isAuth) navigate("/auth");
   }, [isAuth, navigate]);
 
   return (
-    <div className="w-full max-w-screen-xl flex-1 flex-col space-y-6 p-6 mx-auto">
+    <div className="w-full max-w-screen-xl flex-1 flex-col p-6 mx-auto">
       <div
-        className="w-full h-64 rounded-md bg-cover bg-center overflow-hidden"
+        className="w-full h-40 sm:h-64 rounded-md bg-cover bg-center"
         style={{ backgroundImage: `url(${BgLibrary})` }}
-      >
-        <div className="flex gap-x-2">
-          <img
-            className="w-28 h-28 border-solid border-4 rounded-full overflow-hidden"
-            src="https://http.cat/images/200.jpg"
-          />
-          <div className="text-xl font-bold text-white">{userInfo?.email}</div>
-        </div>
-      </div>
+      />
 
-      <ProfileForm userInfo={userInfo!} />
+      <img
+        className="relative -mt-12 sm:-mt-14 mx-auto w-24 h-24 sm:w-28 sm:h-28 border-solid border-4 rounded-full overflow-hidden shadow-md"
+        src="https://http.cat/images/200.jpg"
+        alt="avatar"
+      />
 
-      <Button
-        onClick={() => signOut()}
-        className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
-      >
-        Sign Out
-        <LogOut />
-      </Button>
+      <h1 className="text-2xl text-center mt-2">{userInfo?.email}</h1>
     </div>
   );
 };
