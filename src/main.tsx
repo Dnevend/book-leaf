0e8eaad99ab4fc225711f0b82a95cbca158816b7
Dnevend <Dnevend@gmail.com>
2024-04-18 21:06:18 +0800

    feat: sonner

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/index.tsx";
 import { SupabaseAuthProvider } from "./provider/index.ts";
+import { Toaster } from "@/components/ui/sonner";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <SupabaseAuthProvider>
         <Router />
+        <Toaster />
       </SupabaseAuthProvider>
     </BrowserRouter>
   </React.StrictMode>
